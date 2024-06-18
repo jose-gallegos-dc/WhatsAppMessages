01f996595e909f7f937597870982f92f283964f0
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2024-06-18 12:15:13 -0600

    add key for wca i .env

--- app/WCloudAPI/Templates/HelloWorldTemplate.php
+++ app/WCloudAPI/Templates/HelloWorldTemplate.php
@@ -5,23 +5,24 @@ namespace App\WCloudAPI\Templates;
 class HelloWorldTemplate
 {
 
-    public function __construct() { }
+    public function __construct()
+    {
+    }
 
     /**
-    * Función que arma un array con la plantilla predeterminada hello_world de WhatsAppCloudApi
-    *
-    * @return array Array con la estructura de la plantilla hello_world
-    */
-    public function template() : array 
+     * Función que arma un array con la plantilla predeterminada hello_world de WhatsAppCloudApi
+     *
+     * @return array Array con la estructura de la plantilla hello_world
+     */
+    public function template(): array
     {
         return [
             'template' => [
                 'name' => 'hello_world',
                 'language' => [
-                    'code' => 'en_US',
+                    'code' => 'es_ES',
                 ]
             ]
-        ];    
+        ];
     }
-    
 }
